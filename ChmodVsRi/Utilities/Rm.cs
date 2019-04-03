a360222d62165e4912c3c8b5b5261e5d4890756d
Roman4oo <rom.ivanets@gmail.com>
2019-04-03 09:22:52 +0300

    rm in process

---
+++ ChmodVsRi/Utilities/Rm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChmodVsRi.Utilities
+{
+    class Rm
+    {
+        private string fileName;
+        public void CommandRm(string[] _fileName, FileRepository repo)
+        {
+            fileName = _fileName[0];//in proces 
+
+
+        }
+    }
+}
